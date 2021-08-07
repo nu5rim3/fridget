c10fd2ac2a572f53df2c6e241ae88cd53fb15c00
nu5rim3 <nusrisamath@gmail.com>
2021-08-07 11:21:04 +0530

    updated with BEM

--- src/App.js
+++ src/App.js
@@ -9,33 +9,34 @@ function App() {
     <div className="App">
       <div class="grid grid-cols-12 gap-4">
         <div class="col-start-2 col-span-10">
-          <div className="flex justify-center pt-10 mt-5">
-            <img src={logo} alt="logo" className="h-1/3 w-36" />
+          <div className="Header">
+            <div className="Header__image__div flex justify-center pt-10 mt-5">
+              <img src={logo} alt="logo" className="Header__main--image h-1/3 w-36" />
+            </div>
+
+            <div className="Header__title font-extrabold text-4xl header text-center pt-12">Good morning, Johny!</div>
+            <div className="Header__subtitle font-medium text-sm text-gray-400 text-center pt-3 pb-10">🌤 It's better to go shopping before this friday</div>
           </div>
-
-          <div className="font-extrabold text-4xl header text-center pt-12">Good morning, Johny!</div>
-          <div className="font-medium text-sm text-gray-400 text-center pt-3 pb-10">🌤 It's better to go shopping before this friday</div>
-
-          <form class="w-full bg-white border-gray-800 rounded shadow px-5 py-10">
+          <form class="Card w-full bg-white border-gray-800 rounded shadow px-5 py-10">
             <div class="flex flex-wrap -mx-3 mb-6">
-              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                  <div className="font-semibold text-sm capitalize">🍉 Item Name</div>
+              <div class="Card__input__name w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label class="Card__input__label__name block tracking-wide text-gray-700 font-semibold mb-2 text-sm capitalize" for="grid-first-name">
+                  🍉 Item Name
                 </label>
-                <input class="appearance-none block w-full bg-white text-gray-700 border border-gary-500 rounded py-3 px-4 mb-3 leading-tight focus:border-gary-900" id="grid-item-name" type="text" />
+                <input class="Card__input__name appearance-none block w-full bg-white text-gray-700 border border-gary-500 rounded py-3 px-4 mb-3 leading-tight focus:border-gary-900" id="grid-item-name" type="text" />
               </div>
               <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                  <div className="font-semibold text-sm capitalize">⏰ Expiry Date</div>
+                <label class="Card__input__label__expo block tracking-wide text-gray-700 font-semibold mb-2 text-sm capitalize" for="grid-first-name">
+                  ⏰ Expiry Date
                 </label>
-                <input class="appearance-none block w-full bg-white text-gray-700 border border-gary-500 rounded py-3 px-4 mb-3 leading-tight focus:border-gray-900" id="grid-date" type="date" />
+                <input class="Card__input__exp appearance-none block w-full bg-white text-gray-700 border border-gary-500 rounded py-3 px-4 mb-3 leading-tight focus:border-gray-900" id="grid-date" type="date" />
               </div>
               <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0 py-8">
-                <button class="w-full bg-blue-900 hover:shadow-xl text-white font-bold py-2 px-12 rounded shadow">
+                <button class="Card__button w-full bg-blue-900 hover:shadow-xl text-white font-bold py-2 px-12 rounded shadow">
                   ADD TO FRIDGE
                 </button>
               </div>
-              <p class="text-gray-500 text-xs px-3">⚠️ We don't want more than one piece of the same food in our fridge.</p>
+              <p class="Card__infor text-gray-500 text-xs px-3">⚠️ We don't want more than one piece of the same food in our fridge.</p>
             </div>
           </form>
 
--- src/components/liteItem.js
+++ src/components/liteItem.js
@@ -1,14 +1,14 @@
 const ListItem = ({ name, exp_date, condition }) => {
     return (
-        <div className="list__item">
-            <div className="grid grid-cols-8 gap-4 bg-white border border-gray-300 shadow rounded-md p-4 w-full mx-auto my-3 items-center">
-                <div className="col-span-2 font-semibold">{name}</div>
-                <div className="col-span-4 font-semibold text-gray-400 text-xs p-1">Expiry date —  {exp_date}</div>
-                {/* <div className="col-span-1 font-semibold text-yellow-500 text-xs bg-yellow-200 rounded-xl text-center p-1">Expiring soon</div> */}
-                <div className="col-span-1 font-semibold text-green-500 text-xs bg-green-200 rounded-xl text-center p-1">Healty</div>
-                {/* <div className="col-span-1 font-semibold text-red-500 text-xs bg-red-200 rounded-xl text-center p-1">Expired</div> */}
+        <div className="List">
+            <div className="List__item grid grid-cols-8 gap-4 bg-white border border-gray-300 shadow rounded-md p-4 w-full mx-auto my-3 items-center">
+                <div className="List__item__name col-span-2 font-semibold">{name}</div>
+                <div className="List__item__exp col-span-4 font-semibold text-gray-400 text-xs p-1">Expiry date —  {exp_date}</div>
+                {/* <div className="List__item__expsoon col-span-1 font-semibold text-yellow-500 text-xs bg-yellow-200 rounded-xl text-center p-1">Expiring soon</div> */}
+                <div className="List__item__healty col-span-1 font-semibold text-green-500 text-xs bg-green-200 rounded-xl text-center p-1">Healty</div>
+                {/* <div className="List__item__exp col-span-1 font-semibold text-red-500 text-xs bg-red-200 rounded-xl text-center p-1">Expired</div> */}
                 <div className="col-span-1">
-                    <div className="flex justify-end">
+                    <div className="List__item__button flex justify-end">
                         <button>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
